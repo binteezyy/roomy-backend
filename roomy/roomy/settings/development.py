8e781fa37277fd84f426eb2ab7e484ba023a9cdd
Joshua John Pascual <jjspscl@gmail.com>
2019-12-30 14:10:30 +0800

    debug-toolbar

--- roomy/roomy/settings/development.py
+++ roomy/roomy/settings/development.py
@@ -3,6 +3,10 @@ import os
 
 ALLOWED_HOSTS = ['*', ]
 
+INTERNAL_IPS = [
+    '*',
+    '192.168.1.14'
+]
 DEBUG = True
 
 INSTALLED_APPS += [
@@ -49,6 +53,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
--- roomy/roomy_core/views/base.py
+++ roomy/roomy_core/views/base.py
@@ -1,4 +1,10 @@
 from django.http                        import HttpResponse
+from django.shortcuts                   import render #get_object_or_404, redirect, reverse
+
+context = {
+    "title": "Roomy",
+}
 
 def index(request):
-    return HttpResponse('OK')
+    
+    return render(request,"components/base.html",context)
