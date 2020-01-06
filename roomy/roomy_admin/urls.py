416c35c3d7795d8b2c6abd02052109739870c858
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-01-06 18:53:17 +0800

    layout

--- roomy/roomy_admin/urls.py
+++ roomy/roomy_admin/urls.py
@@ -2,5 +2,12 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from .views import base
 urlpatterns = [
-    path('home/',      base.index,                                        name='index'),
+    path('home/',      base.home,                                        name='home'),
+    path('transaction/',      base.transaction,                                        name='transaction'),
+    path('rental/',      base.rental,                                        name='rental'),
+    path('tenant/',      base.tenant,                                        name='tenant'),
+    path('payment/',      base.payment,                                        name='payment'),
+    path('expense/',      base.expense,                                        name='expense'),
+    path('cashflow/',      base.cashflow,                                        name='cashflow'),
+    path('report/',      base.report,                                        name='report'),
 ]
--- roomy/roomy_admin/views/base.py
+++ roomy/roomy_admin/views/base.py
@@ -7,4 +7,59 @@ context = {
 
 def index(request):
 
-    return render(request,"components/base.html",context)
+    return render(request,"",context)
+
+# home
+def home(request):
+
+    return render(request,"components/home.html",context)
+
+# transaction
+def transaction(request):
+
+    return render(request,"components/transaction.html",context)
+
+# rental
+def rental(request):
+
+    return render(request,"components/rental.html",context)
+
+# tenant
+def tenant(request):
+
+    return render(request,"components/tenant.html",context)
+
+# payment
+def payment(request):
+
+    return render(request,"components/payment.html",context)
+
+# expense
+def expense(request):
+
+    return render(request,"components/expense.html",context)
+
+# cashflow
+def cashflow(request):
+
+    return render(request,"components/cashflow.html",context)
+
+# report
+def report(request):
+
+    return render(request,"components/report.html",context)
+
+# property-management
+def property_management(request):
+
+    return render(request,"components/property-management.html",context)
+
+# room-management
+def room_management(request):
+
+    return render(request,"components/room-management.html",context)
+
+# admin-management
+def admin_management(request):
+
+    return render(request,"components/admin_management.html",context)
