db2d98d0a67f60c28929f6d22c38357fc7ad2855
unknown <alvinpanganiban22@gmail.com>
2020-01-07 21:33:49 +0800

    modified django admin page

--- roomy/roomy/urls.py
+++ roomy/roomy/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +9,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
     urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
+
+admin.site.site_header = "Roomy Admin"
+admin.site.site_title = "Roomy Admin Portal"
+admin.site.index_title = "Welcome to Roomy Admin Portal"
--- roomy/roomy_core/models.py
+++ roomy/roomy_core/models.py
@@ -14,6 +14,7 @@ class Property(models.Model):
 
     class Meta:
         unique_together = ('name', 'street', 'brgy', 'city')
+        verbose_name_plural = 'Properties'
 
 
 class ImageFile(models.Model):
