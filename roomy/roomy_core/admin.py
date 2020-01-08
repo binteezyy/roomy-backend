51124e1273bf308d03090f64f41d51b456150e21
Joshua John Pascual <39576222+jjspscl@users.noreply.github.com>
2020-01-08 10:21:14 +0800

    Rest models (#3)
    * Done models + admin .py
    
    * updated fields. added image field
    
    * modified django admin page
    
    Co-authored-by: Alvin Carlos M. Panganiban <37955300+binteezyy@users.noreply.github.com>

--- roomy/roomy_core/admin.py
+++ roomy/roomy_core/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class PostProperty(admin.ModelAdmin):
+    list_display = ('name', 'street', 'brgy', 'city')
+
+
+class PostRoom(admin.ModelAdmin):
+    list_display = ('property_id', 'floor', 'number')
+
+
+class PostTransaction(admin.ModelAdmin):
+    list_display = ('active', 'room_id', 'start_date')
+
+
+class PostUserAccount(admin.ModelAdmin):
+    list_display = ('user_type', 'user_id', 'transaction_id')
+
+
+class PostBilling(admin.ModelAdmin):
+    list_display = ('paid', 'time_stamp', 'transaction_id')
+
+
+class PostFee(admin.ModelAdmin):
+    list_display = ('description', 'amount')
+
+
+class PostRequest(admin.ModelAdmin):
+    list_display = ('subject', 'transaction_id')
+
+
+class PostImageFile(admin.ModelAdmin):
+    list_display = ('title', 'img_path')
+
+
+admin.site.register(Property, PostProperty)
+admin.site.register(Room, PostRoom)
+admin.site.register(Transaction, PostTransaction)
+admin.site.register(Fee, PostFee)
+admin.site.register(Billing, PostBilling)
+admin.site.register(Request, PostRequest)
+admin.site.register(UserAccount, PostUserAccount)
+admin.site.register(ImageFile, PostImageFile)
