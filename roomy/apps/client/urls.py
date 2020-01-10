affa0dc315e0724baa646a7d50e680629dcfa438
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-01-11 01:54:32 +0800

    updates and changes

--- roomy/apps/client/urls.py
+++ roomy/apps/client/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls.static import static
 from .views import base
 urlpatterns = [
     path('', base.index, name='index'),
+    path('explore/', base.explore, name='explore'),
+    path('about/', base.about, name='about'),
+    path('faq/', base.faq, name='faq'),
 ]
--- roomy/apps/client/views/base.py
+++ roomy/apps/client/views/base.py
@@ -13,3 +13,12 @@ def index(request):
         }
     )
     return render(request,"components/index.html",context)
+
+def explore(request):
+    return render(request,"components/navigation/explore.html",context)
+
+def about(request):
+    return render(request,"components/navigation/about.html",context)
+
+def faq(request):
+    return render(request,"components/navigation/faq.html",context)
