76091031b25f86f7f85f451a98eadf0f3bb68589
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-01-12 01:42:11 +0800

    added some landing pages

--- roomy/apps/client/urls.py
+++ roomy/apps/client/urls.py
@@ -2,8 +2,13 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from .views import base
 urlpatterns = [
-    path('', base.explore, name='explore'),
+    path('explore/', base.explore, name='explore'),
+    path('how_to_book/', base.how_to_book, name='how_to_book'),
+    path('partner_with_us/', base.partner_with_us, name='partner_with_us'),
     path('about/', base.about, name='about'),
     path('faq/', base.faq, name='faq'),
     path('contact/', base.contact, name='contact'),
+    path('terms_of_use/', base.terms_of_use, name='terms_of_use'),
+    path('privacy/', base.privacy, name='privacy'),
+    path('help_center/', base.help_center, name='help_center'),
 ]
--- roomy/apps/client/views/base.py
+++ roomy/apps/client/views/base.py
@@ -6,13 +6,28 @@ context = {
 }
 
 def explore(request):
-    return render(request,"components/navigation/explore.html",context)
+    return render(request,"components/landing_pages/explore.html",context)
+
+def how_to_book(request):
+    return render(request,"components/landing_pages/how_to_book.html",context)
+
+def partner_with_us(request):
+    return render(request,"components/landing_pages/partner_with_us.html",context)
 
 def about(request):
-    return render(request,"components/navigation/about.html",context)
+    return render(request,"components/landing_pages/about.html",context)
 
 def faq(request):
-    return render(request,"components/navigation/faq.html",context)
+    return render(request,"components/landing_pages/faq.html",context)
 
 def contact(request):
-    return render(request,"components/navigation/contact.html",context)
+    return render(request,"components/landing_pages/contact.html",context)
+
+def terms_of_use(request):
+    return render(request,"components/landing_pages/terms_of_use.html",context)
+
+def privacy(request):
+    return render(request,"components/landing_pages/privacy.html",context)
+
+def help_center(request):
+    return render(request,"components/landing_pages/help_center.html",context)
