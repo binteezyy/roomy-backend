ae4b5b8f2ca5f9365569a8921249f8eaf547547d
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-01-14 06:19:53 +0800

    updates & changes

--- roomy/apps/client/urls.py
+++ roomy/apps/client/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path,include
 from django.conf.urls.static import static
-from .views import landing_pages
+from .views import landing
 from .views import property
 
 urlpatterns = [
     #navigation
-    path('home/', landing_pages.home, name='home'),
-    path('explore/', landing_pages.explore, name='explore'),
-    path('booking_guide/', landing_pages.booking_guide, name='booking_guide'),
-    path('partner_with_us/', landing_pages.partner_with_us, name='partner_with_us'),
-    path('about/', landing_pages.about, name='about'),
-    path('faq/', landing_pages.faq, name='faq'),
-    path('contact/', landing_pages.contact, name='contact'),
-    path('terms_of_use/', landing_pages.terms_of_use, name='terms_of_use'),
-    path('privacy/', landing_pages.privacy, name='privacy'),
-    path('help_center/', landing_pages.help_center, name='help_center'),
+    path('home/', landing.home, name='home'),
+    path('explore/', landing.explore, name='explore'),
+    path('booking_guide/', landing.booking_guide, name='booking_guide'),
+    path('partner_with_us/', landing.partner_with_us, name='partner_with_us'),
+    path('about/', landing.about, name='about'),
+    path('faq/', landing.faq, name='faq'),
+    path('contact/', landing.contact, name='contact'),
+    path('terms_of_use/', landing.terms_of_use, name='terms_of_use'),
+    path('privacy/', landing.privacy, name='privacy'),
+    path('help_center/', landing.help_center, name='help_center'),
     #property
     path('explore/property/room', property.room, name='room'),
     path('explore/property/amenities', property.amenities, name='amenities'),
