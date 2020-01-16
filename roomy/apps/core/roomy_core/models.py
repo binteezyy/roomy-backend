a2b01cb669b6ca369fc0d1994339f20f4a159752
unknown <alvinpanganiban22@gmail.com>
2020-01-16 14:09:49 +0800

    media serving fix

--- roomy/apps/core/roomy_core/models.py
+++ roomy/apps/core/roomy_core/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class ImageFile(models.Model):
     title = models.CharField(max_length=32)
     img_path = models.ImageField(
-        upload_to='\media\images', null=True, blank=True)
+        upload_to='images', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title} - {self.img_path}'
