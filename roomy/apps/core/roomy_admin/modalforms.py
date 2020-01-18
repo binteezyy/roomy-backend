0651fde5ca41a7cb6a01f5ba7c4b50eb202e3242
binteezyy <alvinpanganiban22@gmail.com>
2020-01-18 12:50:15 +0800

    minor fix

--- roomy/apps/core/roomy_admin/modalforms.py
+++ roomy/apps/core/roomy_admin/modalforms.py
@@ -61,7 +61,7 @@ class BookingModalForm(BSModalForm):
 class PropertyModalForm(BSModalForm):
     class Meta:
         model = Property
-        exclude = ['property_image', 'proeprty_address.latitude', 'proeprty_address.longitude']
+        exclude = ['property_image',]
 
 
 class RoomModalForm(BSModalForm):
