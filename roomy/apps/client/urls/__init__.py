feba981eecec6240b2ded70f4b74aa047f18aa2c
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-01-20 08:58:08 +0800

    created sign up page

--- roomy/apps/client/urls/__init__.py
+++ roomy/apps/client/urls/__init__.py
@@ -9,10 +9,12 @@ from ..views import *
 
 urlpatterns = [
     #navigation
-
     path('', landing.home, name='home'),
     path('login/',auth.login, name='login'),
     path('logout/', auth.logout, name='logout'),
+    path('sign-up/',auth.sign_up, name='sign_up'),
+    path('forgot-password/', auth.forgot_password, name='forgot_password'),
+    path('get-in-touch/',auth.get_in_touch, name='get_in_touch'),
     path('explore/', explore.index, name='explore'),
     path('booking_guide/', landing.booking_guide, name='booking_guide'),
     path('partner_with_us/', landing.partner_with_us, name='partner_with_us'),
