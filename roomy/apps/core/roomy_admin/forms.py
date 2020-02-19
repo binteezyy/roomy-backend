22f508b8cf80473efb292547c70ad98cd51cf8e0
Joshua <jjspscl@gmail.com>
2020-02-19 22:53:42 +0800

    login fix + notif on frontend

--- roomy/apps/core/roomy_admin/forms.py
+++ roomy/apps/core/roomy_admin/forms.py
@@ -13,16 +13,29 @@ class UserLoginForm(forms.Form):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
 
-        if username and password:
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise forms.ValidationError('User does not exist')
+
+        if not user.check_password(password):
+            raise forms.ValidationError('Incorrect password')
+
+        if user and password:
             user = authenticate(username=username, password=password)
-            if not user:
-                raise forms.ValidationError('User does not exist')
-            if not user.check_password(password):
-                raise forms.ValidationError('Incorrect password')
+
             if not user.is_active:
                 raise forms.ValidationError('User is not active')
+
+            print("VALID USER:",user)
+            return self.cleaned_data
         return super(UserLoginForm, self).clean(*args, **kwargs)
 
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        return user
 
 class UserRegisterForm(forms.ModelForm):
     username = forms.CharField(label='Username')
