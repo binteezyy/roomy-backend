7f13b569ab69cb7aac3da3424b837266c70a1e96
Joshua <jjspscl@gmail.com>
2020-03-02 15:40:34 +0800

    BOOKINGS-MINOR-FIX ( REDIRECT / LOCAL-STATIC /SESSION)

--- roomy/apps/client/views/session.py
+++ roomy/apps/client/views/session.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from django.http                        import HttpResponse
 import json
 def session_clear_form(request):
-    request.session.pop('form_error')
+    if request.session['form_error']: request.session.pop('form_error')
     return HttpResponse({'success': "Success"}, content_type="application/json")
 
 session_urls = [
