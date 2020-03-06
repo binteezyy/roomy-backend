5e0e173f862122ce495f87afd28bb360249ab0b5
Theo Leonard P. Tejada <tlp.tejada@iskolarngbayan.pup.edu.ph>
2020-03-06 19:44:46 +0800

    Account mobile-native endpoint redirection

--- roomy/apps/client/views/account.py
+++ roomy/apps/client/views/account.py
@@ -39,7 +39,10 @@ def BookingView(request,pk):
             "catalog": catalog,
             "room": room,
         })
-        return render(request,"web/components/account/components/booking/view.html",context)
+        if request.user_agent.is_mobile:
+            return render(request,"mobile-native/components/account/components/booking/view.html",context)
+        else:
+            return render(request,"web/components/account/components/booking/view.html",context)
     else:
         return redirect('login')
 
@@ -49,7 +52,10 @@ def saved(request):
         context.update({
             "account_view":"saved",
         })
-        return render(request,"web/components/account/components/saved.html",context)
+        if request.user_agent.is_mobile:
+            return render(request,"mobile-native/components/account/components/saved.html",context)
+        else:
+            return render(request,"web/components/account/components/saved.html",context)
     else:
         return redirect('login')
 
@@ -59,7 +65,10 @@ def messages(request):
         context.update({
             "account_view":"messages",
         })
-        return render(request,"web/components/account/components/messages.html",context)
+        if request.user_agent.is_mobile:
+            return render(request,"mobile-native/components/account/components/messages.html",context)
+        else:
+            return render(request,"web/components/account/components/messages.html",context)
     else:
         return redirect('login')
 
@@ -69,6 +78,9 @@ def profile(request):
         context.update({
             "account_view":"messages",
         })
-        return render(request,"web/components/account/components/profile.html",context)
+        if request.user_agent.is_mobile:
+            return render(request,"mobile-native/components/account/components/profile.html",context)
+        else:
+            return render(request,"web/components/account/components/profile.html",context)
     else:
         return redirect('login')
